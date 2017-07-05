9307cb1e2479e08fefd2b4c3d7dea3acf14e2fd4
wobushifeiwuhehe <991567175@qq.com>
2017-07-05 18:17:14 +0800

    mybatis逆向工程

---
+++ src/dao/TD1O210102Mapper.java
@@ -0,0 +1,9 @@
+package dao;
+
+import model.TD1O210102;
+
+public interface TD1O210102Mapper {
+    int insert(TD1O210102 record);
+
+    int insertSelective(TD1O210102 record);
+}
---
+++ src/model/TD1O210102.java
@@ -0,0 +1,145 @@
+package model;
+
+import java.util.Date;
+
+public class TD1O210102 {
+    private String id;
+
+    private String objid;
+
+    private String equipmentName;
+
+    private String brandNumber;
+
+    private String modelNumber;
+
+    private String mainUser;
+
+    private Date buyDate;
+
+    private String runningState;
+
+    private String verifiedPerson;
+
+    private Date createDate;
+
+    private String createPerson;
+
+    private Date modifyDate;
+
+    private String modifyPerson;
+
+    private String removed;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getObjid() {
+        return objid;
+    }
+
+    public void setObjid(String objid) {
+        this.objid = objid;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public String getBrandNumber() {
+        return brandNumber;
+    }
+
+    public void setBrandNumber(String brandNumber) {
+        this.brandNumber = brandNumber;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getMainUser() {
+        return mainUser;
+    }
+
+    public void setMainUser(String mainUser) {
+        this.mainUser = mainUser;
+    }
+
+    public Date getBuyDate() {
+        return buyDate;
+    }
+
+    public void setBuyDate(Date buyDate) {
+        this.buyDate = buyDate;
+    }
+
+    public String getRunningState() {
+        return runningState;
+    }
+
+    public void setRunningState(String runningState) {
+        this.runningState = runningState;
+    }
+
+    public String getVerifiedPerson() {
+        return verifiedPerson;
+    }
+
+    public void setVerifiedPerson(String verifiedPerson) {
+        this.verifiedPerson = verifiedPerson;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getCreatePerson() {
+        return createPerson;
+    }
+
+    public void setCreatePerson(String createPerson) {
+        this.createPerson = createPerson;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getModifyPerson() {
+        return modifyPerson;
+    }
+
+    public void setModifyPerson(String modifyPerson) {
+        this.modifyPerson = modifyPerson;
+    }
+
+    public String getRemoved() {
+        return removed;
+    }
+
+    public void setRemoved(String removed) {
+        this.removed = removed;
+    }
+}
